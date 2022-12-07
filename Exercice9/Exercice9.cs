4ff566d8d799dd8b6747c5b09cb6478bd76b8582
SylvainTorrenti <torrsyl@gmail.com>
2022-12-07 16:40:13 +0100

    exercice

---
+++ Exercice9/Exercice9.cs
@@ -0,0 +1,15 @@
+﻿int[] Tableau = { 46, 10, 1, 50, 8, 12, 15, 33 };
+int Temp;
+
+for (int i = 0; i < Tableau.Length - 1; i++)
+{
+    while (Tableau[i] > Tableau[i + 1])
+    {
+        Temp = Tableau[i + 1];
+        Tableau[i + 1] = Tableau[i];
+        Tableau[i] = Temp;
+        
+
+    }
+    Console.WriteLine(Tableau[i]);
+}
