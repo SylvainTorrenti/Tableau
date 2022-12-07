c10bd6677e54b80d182658e6cbfef5288d3178ee
SylvainTorrenti <torrsyl@gmail.com>
2022-12-07 19:56:07 +0100

    exercice

--- Exercice9/Exercice9.cs
+++ Exercice9/Exercice9.cs
@@ -1,15 +1,21 @@
 ﻿int[] Tableau = { 46, 10, 1, 50, 8, 12, 15, 33 };
 int Temp;
 
-for (int i = 0; i < Tableau.Length - 1; i++)
+//Verifie si le nombre est plus grand
+for (int Indice = Tableau.Length; Indice > 0; Indice--)
 {
-    while (Tableau[i] > Tableau[i + 1])
+    for (int i = 0; i < Indice - 1; i++)
     {
-        Temp = Tableau[i + 1];
-        Tableau[i + 1] = Tableau[i];
-        Tableau[i] = Temp;
-        i = 0;
-
+        if (Tableau[i] > Tableau[i + 1]) //permet la permutation si le chiffre est plus grand
+        {
+            Temp = Tableau[i];
+            Tableau[i] = Tableau[i + 1];
+            Tableau[i + 1] = Temp;
+        }
     }
-        Console.WriteLine(Tableau[i]);
+}
+
+for (int i = 0; i < Tableau.Length; i++)
+{
+    Console.Write(Tableau[i] + " ");
 }
