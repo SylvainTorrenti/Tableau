907ade0087de701bb272e938b028dae4797c4c15
SylvainTorrenti <torrsyl@gmail.com>
2022-12-07 09:02:39 +0100

    exercice

--- Exercice3/Exercice3.cs
+++ Exercice3/Exercice3.cs
@@ -1,1 +1,12 @@
-﻿
+﻿int[] entiers = new int[6];
+
+for (int i = 0; i < entiers.Length; i++)
+{
+    entiers[i] = i * i;
+    Console.WriteLine(entiers[i]);
+}
+
+//for (int i = 0; i < entiers.Length; i++)
+//{
+    
+//}
