3347978ecd77d38b0f214b5bb9061ec994ad82d0
SylvainTorrenti <torrsyl@gmail.com>
2022-12-07 11:56:42 +0100

    exercice

---
+++ Exercice8/Exercice8.cs
@@ -0,0 +1,12 @@
+﻿int[] Tableau1 = { 4, 8, 7, 12 };
+int[] Tableau2 = { 3, 6 };
+int Produit = 0;
+
+for (int i = 0; i < Tableau2.Length; i++)
+{
+    for (int j = 0; j < Tableau1.Length; j++)
+    {
+        Produit = Tableau2[i] * Tableau1[j] + Produit;
+    }
+}
+Console.WriteLine("Le schtroumpf est de " + Produit);
