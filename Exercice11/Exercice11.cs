9f80a50dc3e0a68d077e33dd065c9e541c85b01b
SylvainTorrenti <torrsyl@gmail.com>
2022-12-09 12:31:10 +0100

    exercice

---
+++ Exercice11/Exercice11.cs
@@ -0,0 +1,5 @@
+﻿int Nombre;
+int[] Tableau1 = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+
+Console.WriteLine("Entrez le nombre à chercher");
+Nombre = int.Parse(Console.ReadLine());
