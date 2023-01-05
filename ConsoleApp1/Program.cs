473ed54101e64a6c9913ce26ac334d274c00162a
SylvainTorrenti <torrsyl@gmail.com>
2023-01-05 16:13:44 +0100

    exercice

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Solution
+{
+    public static double ClosestToZero(double[] ts)
+    {
+        if (ts.Length == 0) return 0;
+        var closest = ts[0];
+        foreach (double d in ts)
+        {
+            double abs = Math.Abs(d);
+            double absClosest = Math.Abs(closest);
+            if (abs < absClosest)
+            {
+                closest = d;
+            }
+            else if (abs == absClosest && closest < 0)
+            {
+                closest = d;
+            }
+        }
+
+        return closest;
+    }
+}
